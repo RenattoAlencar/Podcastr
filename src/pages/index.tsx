470e6ecc8b577fc1647f044f52f6e92fadc936d4
RenattoAlencar <alencar.renato@yahoo.com.br>
2021-05-17 16:29:45 -0300

    feat(SSG, axios, ConvertDateToString)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,59 @@
-import { Header } from "../components/Header";
+import { GetStaticProps } from "next"
+import api from "../services/api"
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString"
 
-export default function Home() {
+type Episode = {
+  id: string,
+  title: string
+  members: string
+  published_at: string
+}
+
+type HomeProps = {
+  episodes: Array<Episode>
+}
+
+export default function Home(props: HomeProps) {
+
+
   return (
-    <h1>index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
 }
 
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR
+      }),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      duration: Number(episode.file.duration),
+      url: episode.file.duration
+    }
+  })
 
+  return {
+    props: {
+      episodes
+    },
+    revalidate: 60 * 60 * 8
+  }
+}
